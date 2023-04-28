1e3c1286aaa38afdf6902b7879cc33ae26541006
johnedisc <christopher.johnedis@gmail.com>
2023-04-28 12:57:10 -0700

    update server.ts
    rewriting functions with async

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,44 @@
-import fs from 'fs';
+import fsPromises from 'fs/promises';
 import path from 'path';
 
-// use path module to make versatile path to file
-// this runs async in node by default. thus file stream stuff will print last
-fs.readFile(path.join(__dirname, 'files', 'queBola.txt'), 'utf-8',(err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+const fileOps = async () => {
+  try {
+    const data = await fsPromises.readFile(path.join(__dirname, 'files', 'queBola.txt'), 'utf-8')
+    console.log(data);
+    await fsPromises.writeFile(path.join(__dirname, 'files', 'promiseWrite.txt'), data);
+    await fsPromises.appendFile(path.join(__dirname, 'files', 'promiseWrite.txt'), '\n\nalgo nuevo');
+    await fsPromises.rename(path.join(__dirname, 'files', 'promiseWrite.txt'), path.join(__dirname, 'files', 'RewritePromiseWrite.txt'));
 
-fs.writeFile(path.join(__dirname, 'files', 'nuevoArchivo.txt'), 'echame limon',(err) => {
-  if (err) throw err;
-  console.log('esta hecho el writeFile');
-});
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+fileOps();
+
+//// this does the same stuff sync like
+//// use path module to make versatile path to file
+//// node has threading by default? thus file stream stuff will print last
+//fsPromises.readFile(path.join(__dirname, 'files', 'queBola.txt'), 'utf-8',(err, data) => {
+//  if (err) throw err;
+//  console.log(data);
+//});
+
+//fsPromises.writeFile(path.join(__dirname, 'files', 'nuevoArchivo.txt'), 'echame limon',(err) => {
+//  if (err) throw err;
+//  console.log('esta hecho el writeFile');
+//});
+//
+//// creates a new file if doesnt yet exist
+//fsPromises.appendFile(path.join(__dirname, 'files', 'loMasNuevoArchivo.txt'), '\n\notravez',(err) => {
+//  if (err) throw err;
+//  console.log('esta hecho el appendFile');
+//});
+//
+//fsPromises.rename(path.join(__dirname, 'files', 'loMasNuevoArchivo.txt'), path.join(__dirname, 'files', 'loMasNuevoBienLlamdo.txt'), (err) => {
+//  if (err) throw err;
+//  console.log('esta hecho el renameFile');
+//});
 
 console.log('ya ves?');
 
