9cc1435593b95e1baa269892059e5a397f311f4b
johnedisc <christopher.johnedis@gmail.com>
2023-04-28 15:04:37 -0700

    update stream.ts
    use read/write streams to write to files or a pipe

--- server/src/stream.ts
+++ server/src/stream.ts
@@ -1,4 +1,20 @@
 import fs from 'fs';
 
 
-const readStream = fs.create
+const readStream = fs.createReadStream('./src/files/discografica.txt', {encoding: 'utf-8'});
+
+const writeStream = fs.createWriteStream('./src/files/copiaDeDisco2.txt');
+
+
+////listen for data input
+//
+//readStream.on('data', (dataChunk) => {
+//  writeStream.write(dataChunk);
+//  console.log('se me fue la mano');
+//});
+//
+
+//or pipe it 
+
+console.log('si');
+readStream.pipe(writeStream);
