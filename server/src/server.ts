31196b3865835b15a4dd84fe6eac43f86dcdb8f0
johnedisc <christopher.johnedis@gmail.com>
2023-04-28 12:20:54 -0700

    update server.ts
    practice writeFile/readFile

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import fs from 'fs';
+import path from 'path';
 
-fs.readFile('./src/files/que.txt', 'utf-8',(err, data) => {
+// use path module to make versatile path to file
+// this runs async in node by default. thus file stream stuff will print last
+fs.readFile(path.join(__dirname, 'files', 'queBola.txt'), 'utf-8',(err, data) => {
   if (err) throw err;
   console.log(data);
 });
+
+fs.writeFile(path.join(__dirname, 'files', 'nuevoArchivo.txt'), 'echame limon',(err) => {
+  if (err) throw err;
+  console.log('esta hecho el writeFile');
+});
+
+console.log('ya ves?');
 
 process.on('uncaughtException', err => {
   console.error(`tremendo lio: ${err}`);
