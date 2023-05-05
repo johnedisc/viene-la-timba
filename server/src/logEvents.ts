697a3b4cce7392921366be8afc0a27748b5bd465
johnedisc <christopher.johnedis@gmail.com>
2023-05-04 21:36:16 -0700

    update logEvents
    find issue. directory mispelled. study the eventemitter class

--- server/src/logEvents.ts
+++ server/src/logEvents.ts
@@ -1,16 +1,15 @@
 import { format } from 'date-fns';
 import { v4 } from 'uuid';
-import fs from 'fs';
 import fsPromises from 'fs/promises';
 import path from 'path';
 
-export const logEvents = async (message: string) => {
-  const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
-  const logItem = `${dateTime}\t${v4()}\t${message}`;
+export const logEvents = async (message: string): Promise<void> => {
+  const dateTime: string = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
+  const logItem: string = `${dateTime}\t${v4()}\t${message}`;
   console.log(logItem);
   try {
     await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), logItem);
   } catch (error) {
-    console.error(error);
+    console.error("the append didn't work",error);
   }
 }
