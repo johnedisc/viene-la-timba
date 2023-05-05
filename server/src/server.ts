a0b07316d7da69cdbdf5f820490d229654c972b3
johnedisc <christopher.johnedis@gmail.com>
2023-05-04 23:12:20 -0700

    update server.ts
    server is running on 3500!

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,40 @@
+import http, { request } from "http";
+import path from "path";
+import fs from "fs";
+import fsPromises from "fs/promises";
+
 import { logEvents } from "./logEvents";
 import EventEmitter from "events";
 
-class TimbaEmitter extends EventEmitter {};
-
-
+class Emitter extends EventEmitter {};
 // initialize object
-const timbaEmitter = new TimbaEmitter();
+const timbaEmitter = new Emitter();
+
+const PORT = process.env.PORT || 3500;
+
+
+const server = http.createServer((request, response) => {
+  console.log(request.url, request.method);
+});
+
+server.listen(PORT, () => console.log(`server running on port: ${PORT}`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // listener for log event
-timbaEmitter.on('log', (msg) => logEvents(msg));
-
-setTimeout(() => {
-  // emit event
-  timbaEmitter.emit('log', 'log event emitted');
-}, 3000)
+//timbaEmitter.on('log', (msg) => logEvents(msg));
+//
+//  timbaEmitter.emit('log', 'log event emitted');
