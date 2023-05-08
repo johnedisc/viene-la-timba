cd37b319787e5dbf08a4a8c7d63af716008d4b5c
johnedisc <christopher.johnedis@gmail.com>
2023-05-08 14:32:13 -0700

    add upcomingGigs
    for api calls

--- viene-la-timba-client/src/App.tsx
+++ viene-la-timba-client/src/App.tsx
@@ -1,10 +1,14 @@
+import UpcomingGigs from './components/UpcomingGigs';
 
 function App() {
 
   return (
     <>
+      <UpcomingGigs />
     </>
   )
 }
 
 export default App
+
+//  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
---
+++ viene-la-timba-client/src/components/UpcomingGigs.tsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+export const UpcomingGigs = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [gigs, setGigs] = useState([]);
+}
