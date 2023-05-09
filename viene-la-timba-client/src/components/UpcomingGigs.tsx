be6988ba89e443c88a2db2575486f9f874545b0b
johnedisc <christopher.johnedis@gmail.com>
2023-05-08 22:58:54 -0700

    update upcomingGig
    print upcoming portland area concerts to screen

--- viene-la-timba-client/src/components/UpcomingGigs.tsx
+++ viene-la-timba-client/src/components/UpcomingGigs.tsx
@@ -5,29 +5,54 @@ export const UpcomingGigs = () => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [gigs, setGigs] = useState([]);
 
+  const errorStyle = {
+    color: "yellow"
+  }
+
+  const divStyle = {
+    padding: "1em 0"
+  }
+
   useEffect(() => {
-      fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${import.meta.env.VITE_TICKETMASTER_KEY}`)
+      fetch(`https://app.ticketmaster.com/discovery/v2/events.json?dmaId=362&size=50&apikey=${import.meta.env.VITE_TICKETMASTER_KEY}`)
         .then(response => {
           if (!response.ok) {
             throw new Error(`error. ${response.status}\n${response.statusText}\n${response.headers}\n${response.url}`);
           }
-          console.log(response);
+//          console.log(response);
           return response.json();
         })
         .then((jsonifiedResponse) => {
-          console.log(jsonifiedResponse);
-          setGigs(jsonifiedResponse.results)
+          setGigs(jsonifiedResponse["_embedded"].events)
           setIsLoaded(true)
         })
         .catch((error) => {
-          console.log(`\n${error.name}: ${error.message}.`);
+//          console.log(`\n${error.name}: ${error.message}.`);
           setError(error)
           setIsLoaded(true)
         });
   }, [])
 
-  return (
-    <>
-    </>
-  );
+
+
+  if (error) {
+    return <h1 style={errorStyle}>error: {error}</h1>
+  } else if (!isLoaded) {
+    return <h1>...loading...</h1>;
+  } else {
+    
+    return (
+      <>
+        <h2>upcoming gigs</h2>
+        {gigs.map((gig: any, index) =>
+          <div key={index} style={divStyle}>
+            <h3>{gig.name}</h3>
+            <h5>{gig.dates.start.localDate}</h5>
+            <h5>{gig["_embedded"].venues[0].name}</h5>
+          </div>
+        )}
+      </>
+    );
+  }
+
 }
