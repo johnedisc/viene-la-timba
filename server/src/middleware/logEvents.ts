44aebf2e41c26524f03aab47a332e463226f84ad
johnedisc <christopher.johnedis@gmail.com>
2023-05-12 10:24:17 -0700

    update server, logEvents
    log out request information

--- server/src/middleware/logEvents.ts
+++ server/src/middleware/logEvents.ts
@@ -7,12 +7,12 @@ import path from 'path';
 export const logEvents = async (message: string, logName: string): Promise<void> => {
   const dateTime: string = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
   const logItem: string = `${dateTime}\t${v4()}\t${message}\n`;
-  console.log(logItem);
+  console.log('this is the log ',logItem);
   try {
-    if (!fs.existsSync(path.join(__dirname, 'logs'))) {
-      await fsPromises.mkdir(path.join(__dirname, 'logs'));
+    if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
+      await fsPromises.mkdir(path.join(__dirname, '..', 'logs'));
     }
-    await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem);
+    await fsPromises.appendFile(path.join(__dirname, '..', 'logs',  logName), logItem);
   } catch (error) {
     console.error("the append didn't work",error);
   }
